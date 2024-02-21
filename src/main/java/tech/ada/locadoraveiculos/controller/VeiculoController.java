3d31755b76dc033918e7e7f28d47ab1df842d481
Alex Araujo <alexsaspbr@gmail.com>
2024-02-21 20:56:57 -0300

    add tela de veiculos

--- src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
+++ src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
@@ -2,6 +2,7 @@ package tech.ada.locadoraveiculos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import tech.ada.locadoraveiculos.service.VeiculoService;
 
 @Controller     //para MVC
@@ -10,5 +11,12 @@ public class VeiculoController {
     @Autowired
     private VeiculoService veiculoService;
 
+    @GetMapping("/veiculos")
+    public String veiculos() {
+
+
+        return "veiculos";
+
+    }
 
 }
