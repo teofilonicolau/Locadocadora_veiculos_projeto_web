9fff9fb71cb7496f062f1dfdd0d8f379cdb70f51
Alex Araujo <alexsaspbr@gmail.com>
2024-02-21 23:10:26 -0300

    exercicio para criar veiculo

--- src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
+++ src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
@@ -2,7 +2,12 @@ package tech.ada.locadoraveiculos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import tech.ada.locadoraveiculos.model.Veiculo;
 import tech.ada.locadoraveiculos.service.VeiculoService;
@@ -25,6 +30,25 @@ public class VeiculoController {
 
         return modelAndView;
 
+    }
+
+    @GetMapping("/veiculo/add")
+    public String abrirTelaAddVeiculo(Model model, Veiculo veiculo) {
+
+        model.addAttribute("veiculo", veiculo);
+
+        return "veiculo-add";
+
+    }
+
+    @PostMapping("/veiculo/add")
+    public String criarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo,
+                               BindingResult result,
+                               Model model) {
+
+
+
+        return "redirect:/veiculos";
     }
 
 }
