c809422f7f2881e1e7a77eb14a5f05f2d88a885c
Alex Araujo <alexsaspbr@gmail.com>
2024-02-21 21:52:16 -0300

    tela de add veiculos

--- src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
+++ src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
@@ -3,7 +3,11 @@ package tech.ada.locadoraveiculos.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import tech.ada.locadoraveiculos.model.Veiculo;
 import tech.ada.locadoraveiculos.service.VeiculoService;
+
+import java.util.List;
 
 @Controller     //para MVC
 public class VeiculoController {
@@ -12,10 +16,14 @@ public class VeiculoController {
     private VeiculoService veiculoService;
 
     @GetMapping("/veiculos")
-    public String veiculos() {
+    public ModelAndView veiculos() {
 
+        List<Veiculo> veiculos = this.veiculoService.listarTodos();
 
-        return "veiculos";
+        ModelAndView modelAndView = new ModelAndView("veiculos");
+        modelAndView.addObject("lista_veiculos", veiculos);
+
+        return modelAndView;
 
     }
 
