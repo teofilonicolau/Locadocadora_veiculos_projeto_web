e70bf16547968ff9378eb6ad4f0db42f11143149
Alex Araujo <alexsaspbr@gmail.com>
2024-02-23 19:56:04 -0300

    projeto locadora de veiculos completo

--- src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
+++ src/main/java/tech/ada/locadoraveiculos/controller/VeiculoController.java
@@ -1,53 +1,78 @@
 package tech.ada.locadoraveiculos.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import tech.ada.locadoraveiculos.model.Veiculo;
 import tech.ada.locadoraveiculos.service.VeiculoService;
 
-import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
-@Controller     //para MVC
+@Controller
 public class VeiculoController {
 
     @Autowired
     private VeiculoService veiculoService;
 
     @GetMapping("/veiculos")
-    public ModelAndView veiculos() {
-
-        List<Veiculo> veiculos = this.veiculoService.listarTodos();
-
+    public ModelAndView veiculos(
+            @RequestParam(defaultValue = "1", value = "page") Integer numeroPagina,
+            @RequestParam(defaultValue = "3", value = "size") Integer tamanhoPagina) {
+        //Model, ModelMap e ModelAndView
         ModelAndView modelAndView = new ModelAndView("veiculos");
-        modelAndView.addObject("lista_veiculos", veiculos);
-
+        Page<Veiculo> veiculoPage = this.veiculoService.listarPaginado(numeroPagina-1, tamanhoPagina);
+        modelAndView.addObject("veiculos", veiculoPage.getContent());
+        modelAndView.addObject("totalPages", veiculoPage.getTotalPages());
+        modelAndView.addObject("currentPage", numeroPagina);
+        modelAndView.addObject("pageSize", veiculoPage.getSize());
         return modelAndView;
-
     }
 
     @GetMapping("/veiculo/add")
-    public String abrirTelaAddVeiculo(Model model, Veiculo veiculo) {
-
-        model.addAttribute("veiculo", veiculo);
-
+    public String addVeiculo(Model model, Veiculo veiculo) {
+        model.addAttribute("add", Boolean.TRUE);
+        model.addAttribute("veiculo", Objects.nonNull(veiculo) ? veiculo : new Veiculo());
         return "veiculo-add";
-
     }
 
     @PostMapping("/veiculo/add")
-    public String criarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo,
+    public String criarVeiculo(@Valid @ModelAttribute("veiculo") Veiculo veiculo,
                                BindingResult result,
                                Model model) {
 
+        if(result.hasErrors()) {
+            return addVeiculo(model, veiculo);
+        }
 
+        this.veiculoService.createVeiculo(veiculo);
+        return "redirect:/veiculos";
+    }
 
+    @GetMapping("/veiculo/{veiculoId}/delete")
+    public String deletarVeiculo(@PathVariable("veiculoId") Long veiculoId) {
+        this.veiculoService.removerVeiculoPorId(veiculoId);
+        return "redirect:/veiculos";
+    }
+
+    @GetMapping("/veiculo/{veiculoId}/edit")
+    public String mostrarEdicaoVeiculo(Model model, @PathVariable("veiculoId") Long veiculoId) {
+        Optional<Veiculo> optionalVeiculo = this.veiculoService.buscarVeiculoPorId(veiculoId);
+        optionalVeiculo.ifPresent(veiculo -> model.addAttribute("veiculo", veiculo));
+        model.addAttribute("add", Boolean.FALSE);
+        return "veiculo-add";
+    }
+
+    @PutMapping("/veiculo/{veiculoId}/edit")
+    public String editarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo,
+                                @PathVariable("veiculoId") Long veiculoId) {
+        veiculo.setId(veiculoId);
+        this.veiculoService.createVeiculo(veiculo);
         return "redirect:/veiculos";
     }
 
--- src/main/java/tech/ada/locadoraveiculos/model/Veiculo.java
+++ src/main/java/tech/ada/locadoraveiculos/model/Veiculo.java
@@ -1,26 +1,35 @@
 package tech.ada.locadoraveiculos.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_veiculo")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Veiculo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "Obrigatório o preenchimento da placa")
+    @NotBlank(message = "Não pode ser vazio")
+    @Size(min = 7, message = "Placa deve conter no minimo 7 caracteres")
     @Column(unique = true)
     private String placa;
     private String marca;
+
     private String modelo;
+
     private String tipo;
+
     private Boolean disponivel;
 
 }
